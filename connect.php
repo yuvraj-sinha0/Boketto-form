888981b963ae799bd724b1d3d4726911d38ae5b5
Yuvraj Sinha <123964429+yuvraj-sinha0@users.noreply.github.com>
2023-09-19 17:05:09 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$number = $_POST['number'];
+
+	// Database connection
+	$conn = new mysqli('sql101.infinityfree.com','if0_35065466','Rp5UWZ1mhY4lB','if0_35065466_XXX');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
